d96391a9b6e7fe7810af8bd653cead95d36c2f05
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-15 00:28:38 +0200

    Create web3.js

---
+++ client_todo_list/src/web3.js
@@ -0,0 +1,152 @@
+import Web3 from "web3";
+
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+const contractAddress = "0x21896Ec38925d005C9A1D109c9515A2B50f43eb7";
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "taskCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "tasks",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "content",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "isImportant",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "completed",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [],
+    name: "totalTaskCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_content",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "_isImportant",
+        type: "bool",
+      },
+    ],
+    name: "createTask",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_taskId",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_title",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_content",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "_isImportant",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "_completed",
+        type: "bool",
+      },
+    ],
+    name: "updateTask",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_taskId",
+        type: "uint256",
+      },
+    ],
+    name: "deleteTask",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const todoListContract = new web3.eth.Contract(abi, contractAddress);
+
+export { web3, todoListContract };
