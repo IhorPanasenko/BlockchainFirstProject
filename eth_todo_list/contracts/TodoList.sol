00fa4177d1b5fc44cfff21910a3da6669de491be
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-15 00:27:36 +0200

    Update todoList logic

--- eth_todo_list/contracts/TodoList.sol
+++ eth_todo_list/contracts/TodoList.sol
@@ -2,23 +2,60 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-  uint public taskCount  = 0;
 
-  struct Task {
-    uint id;
-    string content;
-    bool completed;
-  }
+    uint public taskCount = 0;
+    uint public totalTaskCount = 0;
 
-  mapping(uint => Task) public tasks;
+    struct Task {
+        uint id;
+        string title;
+        string content;
+        bool isImportant;
+        bool completed;
+    }
 
-  constructor() {
-    createTask("This is first task");
-  }
+    mapping(uint => Task) public tasks;
 
-  function createTask(string memory _content) public{
-    taskCount++;
-    tasks[taskCount] = Task(taskCount, _content, false);
-  }
+   constructor() {
+        createTask("This is the first task", "Sample Content", true);
+    }
 
+    function createTask(
+        string memory _title,
+        string memory _content,
+        bool _isImportant
+    ) public {
+        taskCount++;
+        totalTaskCount++;
+        tasks[taskCount] = Task(
+            taskCount,
+            _title,
+            _content,
+            _isImportant,
+            false
+        );
+    }
+
+    function updateTask(
+        uint _taskId,
+        string memory _title,
+        string memory _content,
+        bool _isImportant,
+        bool _completed
+    ) public {
+        require(_taskId > 0 && totalTaskCount >= _taskId);
+
+        tasks[_taskId].title = _title;
+        tasks[_taskId].content = _content;
+        tasks[_taskId].isImportant = _isImportant;
+        tasks[_taskId].completed = _completed;
+    }
+
+    function deleteTask(uint _taskId) public {
+        require(_taskId > 0 && totalTaskCount >= _taskId);
+
+        tasks[_taskId] = tasks[taskCount];
+        delete tasks[taskCount];
+        taskCount--;
+    }
 }
