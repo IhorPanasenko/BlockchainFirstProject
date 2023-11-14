460262484d7326fd42cd3ac7a91a4ea49c393d73
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-15 00:28:02 +0200

    Write tests for contract

---
+++ eth_todo_list/test/todo_list_test.js
@@ -0,0 +1,43 @@
+const TodoList = artifacts.require("TodoList");
+
+contract("TodoList", (accounts) => {
+  let todoListInstance;
+
+  before(async () => {
+    todoListInstance = await TodoList.deployed();
+  });
+
+  it("should create a new task", async () => {
+    const initialTaskCount = await todoListInstance.taskCount();
+
+    await todoListInstance.createTask("New Task Title", "New Task Content", true);
+
+    const updatedTaskCount = await todoListInstance.taskCount();
+    assert.equal(updatedTaskCount.toNumber(), initialTaskCount.toNumber() + 1, "Task count should increase by 1");
+  });
+
+  it("should update a task", async () => {
+    const taskId = 1; // Assuming there is at least one task in the TodoList
+
+    const originalTask = await todoListInstance.tasks(taskId);
+
+    await todoListInstance.updateTask(taskId, "Updated Title", "Updated Content", false, true);
+
+    const updatedTask = await todoListInstance.tasks(taskId);
+    assert.equal(updatedTask.title, "Updated Title", "Title should be updated");
+    assert.equal(updatedTask.content, "Updated Content", "Content should be updated");
+    assert.equal(updatedTask.isImportant, false, "isImportant should be updated");
+    assert.equal(updatedTask.completed, true, "completed should be updated");
+  });
+
+  it("should delete a task", async () => {
+    const taskId = 1; // Assuming there is at least one task in the TodoList
+
+    const initialTaskCount = await todoListInstance.taskCount();
+
+    await todoListInstance.deleteTask(taskId);
+
+    const updatedTaskCount = await todoListInstance.taskCount();
+    assert.equal(updatedTaskCount.toNumber(), initialTaskCount.toNumber() - 1, "Task count should decrease by 1");
+  });
+});
