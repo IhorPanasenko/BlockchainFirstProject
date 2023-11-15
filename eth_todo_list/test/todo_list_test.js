4cd2ac188bcc036e8ba1d14fcee8d0fdcfe6b6f6
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-15 15:23:31 +0200

    Update todo_list_test.js

--- eth_todo_list/test/todo_list_test.js
+++ eth_todo_list/test/todo_list_test.js
@@ -9,35 +9,36 @@ contract("TodoList", (accounts) => {
 
   it("should create a new task", async () => {
     const initialTaskCount = await todoListInstance.taskCount();
+    await todoListInstance.createTask("New Task", "Task Content", false);
+    const newTaskCount = await todoListInstance.taskCount();
 
-    await todoListInstance.createTask("New Task Title", "New Task Content", true);
-
-    const updatedTaskCount = await todoListInstance.taskCount();
-    assert.equal(updatedTaskCount.toNumber(), initialTaskCount.toNumber() + 1, "Task count should increase by 1");
+    assert.equal(newTaskCount, initialTaskCount.toNumber() + 1, "Task count should increase by 1");
   });
 
-  it("should update a task", async () => {
-    await todoListInstance.createTask("New Task Title", "New Task Content", true);
-    const taskId = 1;
+  it("should update an existing task", async () => {
+    const taskId = 1; // Assuming there's at least one task created in the constructor
+    const newTitle = "Updated Task";
+    const newContent = "Updated Content";
+    const isImportant = false;
+    const completed = true;
 
-    const originalTask = await todoListInstance.tasks(taskId);
-    await todoListInstance.updateTask(taskId, "Updated Title", "Updated Content", false, true);
+    await todoListInstance.updateTask(taskId, newTitle, newContent, isImportant, completed);
 
     const updatedTask = await todoListInstance.tasks(taskId);
-    assert.equal(updatedTask.title, "Updated Title", "Title should be updated");
-    assert.equal(updatedTask.content, "Updated Content", "Content should be updated");
-    assert.equal(updatedTask.isImportant, false, "isImportant should be updated");
-    assert.equal(updatedTask.completed, true, "completed should be updated");
+
+    assert.equal(updatedTask.title, newTitle, "Task title should be updated");
+    assert.equal(updatedTask.content, newContent, "Task content should be updated");
+    assert.equal(updatedTask.isImportant, isImportant, "Task importance should be updated");
+    assert.equal(updatedTask.completed, completed, "Task completion status should be updated");
   });
 
-  it("should delete a task", async () => {
-    await todoListInstance.createTask("New Task Title", "New Task Content", true);
-    const taskId = 1;
-
+  it("should delete an existing task", async () => {
+    const taskId = 2; // Assuming there's at least two tasks created in the constructor
     const initialTaskCount = await todoListInstance.taskCount();
+
     await todoListInstance.deleteTask(taskId);
 
-    const updatedTaskCount = await todoListInstance.taskCount();
-    assert.equal(updatedTaskCount.toNumber(), initialTaskCount.toNumber() - 1, "Task count should decrease by 1");
+    const newTaskCount = await todoListInstance.taskCount();
+    assert.equal(newTaskCount, initialTaskCount.toNumber() - 1, "Task count should decrease by 1");
   });
 });
